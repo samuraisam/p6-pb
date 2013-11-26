3ad92cf189b63a696f189b44aeb79b6217d339f1
Geoffrey Broadwell <gjb@google.com>
2013-11-26 09:48:59 -0800

    Track unknown fields in PB::Message base class

--- lib/PB/Message.pm
+++ lib/PB/Message.pm
@@ -1,2 +1,7 @@
+#= Base class for all generated message classes
 class PB::Message {
+    # NOTE: Use at least one hyphen in method and attribute names to ensure
+    #       they won't collide with message field names.
+
+    has @.unknown-fields;
 }
