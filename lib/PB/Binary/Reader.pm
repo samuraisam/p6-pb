a3556166381625cb2c9323bf7414be896a35e5df
Geoffrey Broadwell <gjb@google.com>
2013-11-26 09:49:46 -0800

    Start sketching full-message binary PB reading

--- lib/PB/Binary/Reader.pm
+++ lib/PB/Binary/Reader.pm
@@ -5,6 +5,7 @@ use v6;
 module PB::Binary::Reader;
 
 use PB::Binary::WireTypes;
+use PB::Message;
 
 
 # SECURITY: Depends on the behavior of Blob.[] returning 0 for out of bounds
@@ -22,6 +23,19 @@ class X::PB::Binary::Invalid is Exception {
 }
 
 
+class X::PB::Binary::WireType::Mismatch is Exception {
+    has $.offset;
+    has $.buffer-wiretype;
+    has $.expected-wiretype;
+    has $.message-type;
+    has $.field;
+
+    method message {
+        "Saw wiretype $.buffer-wiretype for{ $.field.pb_packed ?? ' packed' !! '' } field $.field.pb_name (tag ID $.field.pb_number) of message type $.message-type at offset $.offset, but expected wiretype $.expected-wiretype instead.";
+    }
+}
+
+
 #= Convert combined field key to (field tag number, wire type)
 sub decode-field-key(int $key) is pure is export {
     ($key +> 3, $key +& 7)
@@ -31,6 +45,27 @@ sub decode-field-key(int $key) is pure is export {
 #= Decode a zigzag-encoded signed number
 sub decode-zigzag(int $zigzag --> int) is pure is export {
     ($zigzag +> 1) +^ -($zigzag +& 1)
+}
+
+
+#= Decode a value of an arbitrary field type
+sub decode-value(Str $field-type, Mu $value --> Mu) is pure is export {
+    given $field-type {
+        when 'int32'|'uint32'|'fixed32'|'sfixed32'
+            |'int64'|'uint64'|'fixed64'|'sfixed64'
+            |'bytes'           { $value }
+        when 'enum'            { $value }  # XXXX: Decode to real Enum?
+        when 'bool'            { ?$value }
+        when 'sint32'|'sint64' { decode-zigzag($value) }
+        when 'string'          { $value.decode }
+        when 'float'|'double'  {
+            die "XXXX: Don't know how to deal with floating point type '$_'";
+        }
+        default {
+            # XXXX: How to look up message type from string type name?
+            read-message($value, (my $ = 0), PB::Message);  # message-type($field-type));
+        }
+    }
 }
 
 
@@ -73,6 +108,14 @@ sub read-fixed64(blob8 $buffer, Int $offset is rw --> uint64) is export {
 }
 
 
+#= Read a blob8 from a buffer at a given offset with a given length, updating the offset
+sub read-blob8(blob8 $buffer, Int $offset is rw, Int $length --> blob8) is export {
+    my $blob := $buffer.subbuf($offset, $length);
+    $offset  += $length;
+    $blob;
+}
+
+
 #= Read a kv pair from a buffer at a given offset, updating the offset
 sub read-pair(blob8 $buffer, Int $offset is rw) is export {
     my $orig-offset = $offset;
@@ -103,4 +146,41 @@ sub read-pair(blob8 $buffer, Int $offset is rw) is export {
     }
 
     ($field-tag, $wire-type, $value);
+}
+
+
+#= Read an entire message from a buffer at a given offset with an optional maximum length, updating the offset
+sub read-message(PB::Message $message-type, blob8 $buffer, Int $offset is rw,
+                 Int $length?) is export {
+    # For security, if $length is set, decode a sub-buf instead
+    if $length.defined {
+        my $sub-buf := $buffer.subbuf($offset, $length);
+        $offset += @$sub-buf;
+        return read-message($sub-buf, (my $ = 0));
+    }
+
+    my $message = $message-type.new;
+    my %field  := $message-type.^fields-by-tag;
+    while $offset < @$buffer {
+        my $orig-offset = $offset;
+        my ($field-tag, $wire-type, $value) = read-pair($buffer, $offset);
+        my $field = %field{$field-tag};
+
+        if !$field.defined {
+            $message.unknown-fields.push({ :$field-tag, :$wire-type, :$value });
+            next;
+        }
+
+        my $expected-wiretype  = %WIRE_TYPE{$field.pb_type}
+                              // %WIRE_TYPE<DEFAULT>;
+
+        fail X::PB::Binary::WireType::Mismatch.new(
+            :offset($orig-offset), :$field, $:message-type,
+            :buffer-wiretype($wire-type), :$expected-wiretype)
+            unless $wire-type ~~ $expected-wiretype
+                || $wire-type ~~ WireType::LENGTH_DELIMITED
+                   && $field.pb_packed;
+
+        # XXXX: ...
+    }
 }
